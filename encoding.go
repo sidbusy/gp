527905259f1d748920590f4f8794bf05548dfa96
sidbusy <sidbusy@qq.com>
2015-10-18 11:51:26 +0800

    nomsg

--- encoding.go
+++ encoding.go
@@ -7,3 +7,8 @@ import (
 func Base64Encode(data string) string {
 	return base64.StdEncoding.EncodeToString([]byte(data))
 }
+
+func Base64Decode(data string) (string, error) {
+	b, err := base64.StdEncoding.DecodeString(data)
+	return string(b), err
+}
